d6a78fe378131ed0a1f0a1dd3c8362c63b4f9202
内藤芳彦 <uotian@naitouyoshihiko-no-MacBook-Pro.local>
2016-10-23 06:54:49 +0900

    make bingo

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -1,6 +1,18 @@
 class SampleController < ApplicationController
-  VALUES = [1..2]
+  @@max         = 0
+  @@values      = []
+  @@values_done = []
   def index
-    @values = VALUES
+    if params[:max].present?
+      @@max = params[:max].to_i
+      @value = "Start from #{@@max}"
+      @@values = (1..@@max).to_a.shuffle
+      @@values_done = []
+    else
+      @value = @@values.shift
+      @@values_done << @value
+    end
+    @values_done = @@values_done
+    @max = @@max
   end
 end
